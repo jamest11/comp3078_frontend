bbd3f6b8a68e75805c1e3e40204843587be92ee1
jamest11 <jameswtory@gmail.com>
2023-02-19 16:37:07 -0500

    Dynamic nav bar based on user type

--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -1,6 +1,16 @@
 import { AppBar, Toolbar, Button, Typography, Box } from '@mui/material';
+import { useAuth } from 'security/AuthContextProvider';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const { user, handleLogout } = useAuth();
+
+  const logout = () => {
+    handleLogout();
+    navigate('/login');
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -8,9 +18,20 @@ const NavBar = () => {
           Quiztionnaire
         </Typography>
         <Box sx={{ flex: 1, marginLeft: 5 }}>
-          <Button href="/" color="inherit">Home</Button>
-          <Button color="inherit">Grades</Button>
-          <Button href="/quiz" color="inherit">Quizzes</Button>
+          { user ? user.userType === 'instructor' ? (
+            <>
+              <Button component={Link} to="/instructor-quizzes" color="inherit">Quizzes</Button>
+              <Button color="inherit" onClick={logout}>Logout</Button>    
+            </>
+          ) : (
+            <>
+              <Button component={Link} to="/student-profile" color="inherit">Home</Button>
+              <Button color="inherit" onClick={logout}>Logout</Button>            
+            </>
+          ) : (
+            <>
+            </>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
