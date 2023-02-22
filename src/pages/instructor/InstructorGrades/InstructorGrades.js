0025143932279d80b8d76273dbc56d1774ac4d7b
jamest11 <jameswtory@gmail.com>
2023-02-21 21:28:05 -0500

    Create instructor quiz grades table component

--- src/pages/instructor/InstructorGrades/InstructorGrades.js
+++ src/pages/instructor/InstructorGrades/InstructorGrades.js
@@ -1,9 +1,12 @@
-import { Container, Divider, Grid, LinearProgress, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { Container, Grid, LinearProgress, Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 import { instructorApi } from 'services/api';
-import { formatDate } from 'utils';
+import Title from 'components/Title';
 import ClassGrade from './components/ClassGrade';
+import QuizGradesTable from './components/QuizGradesTable';
+import Subtitle from 'components/Subtitle';
+import TitleDivider from 'components/TitleDivider';
 
 const InstructorGrades = () => {
   const [qgLoading, setQgLoading] = useState(true);
@@ -32,44 +35,18 @@ const InstructorGrades = () => {
 
   return (
     <Container maxWidth="lg" sx={{ mt: 2 }}>
-      <Typography variant="h3" gutterBottom>Grades</Typography>
-      <Divider  sx={{ my: 2, boxShadow: 2 }}/>
+      <Title>Grades</Title>
+      <TitleDivider />
 
       <Grid container columnGap={4}>
         <Grid item sm={10} md={7}>
-          <Typography variant="h4" gutterBottom>Quiz Grades</Typography>
+          <Subtitle>Quiz Grades</Subtitle>
 
-          <TableContainer component={Paper}>
-            {qgLoading ? (
-              <LinearProgress />
-            ) : (
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Class</TableCell>
-                  <TableCell>Quiz</TableCell>
-                  <TableCell>Due Date</TableCell>
-                  <TableCell># Completed</TableCell>
-                  <TableCell>Avg. Score</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {quizGrades.map((grade, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{grade.class}</TableCell>
-                    <TableCell>{grade.quiz}</TableCell>
-                    <TableCell>{formatDate(grade.dueDate)}</TableCell>
-                    <TableCell>{grade.completed}</TableCell>
-                    <TableCell>{grade.average}%</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>)}
-          </TableContainer>
+          <QuizGradesTable loading={qgLoading} quizGrades={quizGrades} />
         </Grid>
 
         <Grid item>
-          <Typography variant="h4" gutterBottom>Average Class Grades</Typography>
+          <Subtitle>Average Class Grades</Subtitle>
           {cgLoading ? (
             <LinearProgress />
           ) : (
