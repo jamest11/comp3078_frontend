aac9abfdf85c212dc9c200ba2a6fbafd429bc83a
jamest11 <jameswtory@gmail.com>
2023-02-23 21:31:54 -0500

    Update date formatting with dayjs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,13 @@
+import dayjs from 'dayjs';
+
 const formatTime = (seconds) => {
   return `${Math.floor(seconds / 60)}:` + `${seconds % 60}`.padStart(2, '0');
 };
 
 const formatDate = (dateStr) => {
-  const date = new Date(dateStr);
-  return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`;
+  //const date = new Date(dateStr);
+  //return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`;
+  return dayjs(dateStr).format('YYYY-MM-DD');
 };
 
 const emailValidatorRx = /(?!.*[-_.]{2}.*)^[a-zA-Z\d][a-zA-Z\d._-]+[a-zA-Z\d]@([a-zA-Z\d][a-zA-Z\d-]*[a-zA-Z\d]\.){1,}[a-z]{2,}$/;
