17e492864b82428048727deeafef822e301ba3e4
jamest11 <jameswtory@gmail.com>
2023-03-28 16:30:43 -0400

    Disable react strict mode

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
