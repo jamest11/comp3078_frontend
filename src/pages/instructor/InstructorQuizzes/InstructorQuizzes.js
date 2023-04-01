e78a1356fe3f532de62df8ab15f70180c4a8c7b2
jamest11 <jameswtory@gmail.com>
2023-04-01 14:28:52 -0400

    Add filters to instructor quizzes page

--- src/pages/instructor/InstructorQuizzes/InstructorQuizzes.js
+++ src/pages/instructor/InstructorQuizzes/InstructorQuizzes.js
@@ -1,4 +1,5 @@
-import { Button, Container, Grid, LinearProgress, Stack, Pagination } from '@mui/material';
+import { Button, Container, Grid, LinearProgress, Stack, Pagination, TextField, MenuItem, IconButton } from '@mui/material';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { useEffect, useRef, useState } from 'react';
 
 import { instructorApi } from 'services/api';
@@ -15,7 +16,9 @@ const PAGE_SIZE = 4;
 const InstructorQuizzes = () => {
   const [quizzes, setQuizzes] = useState([]);
   const [scheduledQuizzes, setScheduledQuizzes] = useState([]);
+  const [filteredQuizzes, setFilteredQuizzes] = useState([]);
   const [sqPage, setSqPage] = useState(1);
+  const [sqFilter, setSqFilter] = useState('');
   const [classes, setClasses] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const modalRef = useRef(null);
@@ -23,6 +26,7 @@ const InstructorQuizzes = () => {
   const [loading, setLoading] = useState(true);
 
   const updateScheduledQuizzes = () => {
+    setSqFilter('');
     instructorApi.getScheduledQuizzes('incomplete') 
       .then((res) => setScheduledQuizzes(res.data))
       .catch((error) => console.error('Server error'));
@@ -48,8 +52,23 @@ const InstructorQuizzes = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setSqPage(1);
+    if(sqFilter === '') {
+      setFilteredQuizzes(scheduledQuizzes);
+    }
+    else {
+      const filtered = scheduledQuizzes.filter((quiz) => quiz.classTitle === sqFilter);
+      setFilteredQuizzes(filtered);
+    }
+  }, [scheduledQuizzes, sqFilter]);
+
   const handlePageChange = (event, value) => {
     setSqPage(value);
+  };
+
+  const handleFilterChange = (event) => {
+    setSqFilter(event.target.value);
   };
 
   return (
@@ -74,7 +93,14 @@ const InstructorQuizzes = () => {
             <LinearProgress />
           ) : (
             <Stack spacing={1} sx={{ maxWidth: 500 }}>
-              {scheduledQuizzes.slice((sqPage - 1) * PAGE_SIZE, sqPage * PAGE_SIZE).map((quiz, index) => (
+              <Stack direction="row" alignItems="center">
+                <Pagination 
+                  count={Math.ceil(filteredQuizzes.length / PAGE_SIZE)} 
+                  page={sqPage} 
+                  onChange={handlePageChange} 
+                />
+              </Stack>
+              {filteredQuizzes.slice((sqPage - 1) * PAGE_SIZE, sqPage * PAGE_SIZE).map((quiz, index) => (
                 <ScheduledQuiz 
                   key={index} 
                   data={quiz} 
@@ -82,25 +108,38 @@ const InstructorQuizzes = () => {
                   setShowModal={setShowModal}
                 />
               ))}
-              <Pagination 
-                 count={Math.floor(scheduledQuizzes.length / PAGE_SIZE) > 0 ? Math.floor(scheduledQuizzes.length / PAGE_SIZE) : 1} 
-                page={sqPage} 
-                onChange={handlePageChange} 
-              />
+
+              <Stack direction="row" alignItems="center">
+                <Button 
+                  variant="contained" 
+                  color="success"
+                  onClick={() => {
+                    modalRef.current = null;
+                    setShowModal(true);
+                  }}
+                >
+                  Schedule Quiz
+                </Button>
+
+                <TextField 
+                  sx={{ ml: 'auto', width: 200, backgroundColor: 'white', borderRadius: 1 }}
+                  select 
+                  size="small"
+                  label="Class Filter"
+                  value={sqFilter}
+                  onChange={handleFilterChange}
+                >
+                  {classes.map((item, index) => (
+                    <MenuItem key={index} value={item.title} dense>{item.title}</MenuItem>
+                  ))}
+                </TextField>
+                
+                <IconButton onClick={() => setSqFilter('')} sx={{ p: 0.5 }}>
+                  <HighlightOffIcon />
+                </IconButton>
+              </Stack>
             </Stack>
           )}
-
-          <Button 
-            variant="contained" 
-            color="success"
-            sx={{ mt: 1 }} 
-            onClick={() => {
-              modalRef.current = null;
-              setShowModal(true);
-            }}
-          >
-            Schedule Quiz
-          </Button>
         </Grid>
 
         <Grid item sm={12} md={5}>
--- src/pages/instructor/InstructorQuizzes/components/QuizContainer.js
+++ src/pages/instructor/InstructorQuizzes/components/QuizContainer.js
@@ -1,16 +1,40 @@
-import { Button, LinearProgress, Pagination, Stack } from '@mui/material';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Button, LinearProgress, Pagination, Stack, IconButton, Box, InputAdornment, OutlinedInput, FormControl, InputLabel } from '@mui/material';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import SearchIcon from '@mui/icons-material/Search';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Quiz from './Quiz';
 
 const PAGE_SIZE = 4;
 
 const QuizContainer = ({ quizzes, loading }) => {
-  const navigate = useNavigate();
+  const [filter, setFilter] = useState('');
   const [page, setPage] = useState(1);
+  const [filteredQuizzes, setFilteredQuizzes] = useState([]);
+
+  useEffect(() => {
+    setFilteredQuizzes(quizzes);
+    setPage(1);
+    setFilter('');
+  }, [quizzes]);
 
   const handlePageChange = (event, value) => {
     setPage(value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const updateFilter = () => {
+    setFilteredQuizzes(quizzes.filter((quiz) => quiz.title.toLowerCase().includes(filter.toLowerCase())));
+    setPage(1);
+  };
+
+  const resetFilter = () => {
+    setFilteredQuizzes(quizzes);
+    setFilter('');
+    setPage(1);
   };
 
   return (
@@ -19,26 +43,59 @@ const QuizContainer = ({ quizzes, loading }) => {
         <LinearProgress />
       ) : (
           <Stack spacing={1} sx={{ maxWidth: 500 }}>
-            {quizzes.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE).map((quiz, index) => (
-              <Quiz key={index} data={quiz} />
-            ))}
-
             <Pagination 
-              count={Math.floor(quizzes.length / PAGE_SIZE) > 0 ? Math.floor(quizzes.length / PAGE_SIZE) : 1} 
+              count={Math.ceil(filteredQuizzes.length / PAGE_SIZE)} 
               page={page} 
               onChange={handlePageChange} 
             />
+          
+            {filteredQuizzes.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE).map((quiz, index) => (
+              <Quiz key={index} data={quiz} />
+            ))}
+
+            <Stack direction="row" alignItems="center" justifyContent="space-between">
+              <Button 
+                variant="contained" 
+                color="success"
+                component={Link}
+                to="create-quiz"
+              >
+                Create Quiz
+              </Button>
+
+              <Box sx={{ display: 'flex', alignItems: 'center'}}>
+                <FormControl size="small">
+                  <InputLabel htmlFor="filter">Search</InputLabel>
+                  <OutlinedInput 
+                    type="text" 
+                    id="filter"
+                    value={filter}
+                    onChange={handleFilterChange}
+                    onKeyDown={(event) => {
+                      if (event.key === 'Enter') {
+                        event.preventDefault();
+                        updateFilter();
+                      }
+                    }}
+                    endAdornment={
+                      <InputAdornment position="end" onClick={updateFilter}>
+                        <IconButton edge="end" onClick={updateFilter}>
+                          <SearchIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Search"
+                    sx={{ backgroundColor: 'white', borderRadius: 1, width: 200 }}
+                  />
+                </FormControl>
+
+                <IconButton onClick={resetFilter} sx={{ p: 0.5 }}>
+                  <HighlightOffIcon />
+                </IconButton>
+              </Box>
+            </Stack>
           </Stack>
       )}
-
-      <Button 
-        variant="contained" 
-        color="success"
-        sx={{ mt: 1 }} 
-        onClick={() => navigate('create-quiz')}
-      >
-        Create New Quiz
-      </Button>
     </>
   );
 };
