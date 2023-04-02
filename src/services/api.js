347c5722d0ec1b569e56acf5ae31910be4d56f79
jamest11 <jameswtory@gmail.com>
2023-04-02 17:39:46 -0400

    Fix api URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/',
-  //baseURL: 'https://capstone-backend-36217.herokuapp.com/',
+  //baseURL: 'http://localhost:4000/',
+  baseURL: 'https://capstone-backend-36217.herokuapp.com/',
   headers: { post: { 'Content-Type': 'application/json' } },
   timeout: 3000
 });
